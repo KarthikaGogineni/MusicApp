241c59b7020ccb08f750a3ab5176b490f306e153
KarthikaGogineni <73654045+KarthikaGogineni@users.noreply.github.com>
2024-12-08 17:44:09 -0500

    Login.js

---
+++ src/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
+import { useHistory } from 'react-router-dom';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const history = useHistory();
+
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await Auth.signIn(username, password);
+      console.log('User signed in:', user);
+      history.push('/dashboard'); // Redirect to Dashboard page after successful login
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      <form onSubmit={handleSignIn}>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
